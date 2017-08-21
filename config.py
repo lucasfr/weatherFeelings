9cbc0bcbb8e6f6eaeda944f94d03297af51b852b
lucasfr <lucas.franca.14@ucl.ac.uk>
2017-08-21 15:41:31 -0500

    Secret file with keys

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ DEBUG = True # Turns on debugging features in Flask
 BCRYPT_LEVEL = 12 # Configuration for the Flask-Bcrypt extension
 MAIL_FROM_EMAIL = "robert@example.com" # For use in application emails
 
+from secret import *
 # Perhaps we need those keys in a separate config file and add to .gitignore
-CONSUMER_KEY = 'XXX'
-CONSUMER_SECRET = 'XXX'
-ACCESS_KEY = 'XXX'
-ACCESS_SECRET = 'XXX'
+#CONSUMER_KEY = 'XXX'
+#CONSUMER_SECRET = 'XXX'
+#ACCESS_KEY = 'XXX'
+#ACCESS_SECRET = 'XXX'
